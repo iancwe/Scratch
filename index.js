0e6a81954d62a7219f637a5317dd26149b72527d
iancwe <crubbishhute@gmail.com>
2017-04-12 03:51:42 +0800

    adding in heroku

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const app = express()
-const ejsLayout = ('express-ejs-layouts')
-const port = 3000
+var port = process.env.PORT || 3000
 
-// setting up the layout and template engine for express
+// setting my template engine for express
 app.set('view engine', 'ejs')
+
+// setting the layout structure
+var ejsLayout = require('express-ejs-layouts')
 app.use(ejsLayout)
+app.use(express.static('assets'))
 
 // this is the main page
 app.get('/', function (req, res) {
   res.render('landing')
 })
 
+// this is the main page
+app.get('/home', function (req, res) {
+  res.render('home')
+})
+
 // To make sure we are connected to heroku or localhost
-.app.listen(port, function () {
+app.listen(port, function () {
   console.log('express is running on port ' + port)
 })
