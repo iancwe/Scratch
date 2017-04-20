ec36a987c55f5d983429be40c039ee3eaa8c979c
iancwe <crubbishhute@gmail.com>
2017-04-20 15:00:07 +0800

    fixing heroku api issue

--- controllers/overlay.js
+++ controllers/overlay.js
@@ -61,24 +61,25 @@ const proList = {
       if (err) {
         req.flash('error', 'Can\'t populate user portfolio')
         req.redirect('/home')
-      } else {
+      } else if (company.length > 0) {
 
-        res.render('home', {companies: company, dAvg: {} })
-        // let comAvg = {}
-        // company.forEach(function (nth, i) {
-        //   console.log('I am here', nth)
-        //   let comSym = company[i].symbol
-        //   let url = 'http://www.alphavantage.co/query?function=SMA&symbol=' + comSym + '&interval=daily&time_period=2&series_type=close&apikey=C8VN'
-        //   unirest.get(url).end(function (output) {
-        //     let data = output.body['Technical Analysis: SMA']
-        //     let avg = data[Object.keys(data)[0]]
-        //     console.log('igiojog', avg.SMA)
-        //     comAvg[company[i]._id] = (avg.SMA)
-        //     if (Object.keys(comAvg).length === company.length) {
-        //       res.render('home', {companies: company, dAvg: comAvg})
-        //     }
-        //   })
-        // })
+        let comAvg = {}
+        company.forEach(function (nth, i) {
+          console.log('I am here', nth)
+          let comSym = company[i].symbol
+          let url = 'http://www.alphavantage.co/query?function=SMA&symbol=' + comSym + '&interval=daily&time_period=2&series_type=close&apikey=C8VN'
+          unirest.get(url).end(function (output) {
+            let data = output.body['Technical Analysis: SMA']
+            let avg = data[Object.keys(data)[0]]
+            console.log('igiojog', avg.SMA)
+            comAvg[company[i]._id] = (avg.SMA)
+            if (Object.keys(comAvg).length === company.length) {
+              res.render('home', {companies: company, dAvg: comAvg})
+            }
+          })
+        })
+      } else {
+        res.render('home', {companies: company, dAvg: {}})
       }
     })
   },
